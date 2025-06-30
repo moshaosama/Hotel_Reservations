319af7e55e321299c8457180677ad6654198942f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-30 15:03:48 +0300

    add: new NavbarLinks component file

---
+++ zendo_fe/src/Data/NavbarLinks.tsx
@@ -0,0 +1,32 @@
+import { FaCarSide } from "react-icons/fa";
+import { GrVisa } from "react-icons/gr";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+import { LuHotel } from "react-icons/lu";
+import { MdOutlineFlight } from "react-icons/md";
+
+export const NavbarLinks = [
+  {
+    icon: <MdOutlineFlight />,
+    name: "Flights",
+  },
+  {
+    icon: <LuHotel />,
+    name: "Hotels",
+  },
+  {
+    icon: <LiaShoppingBagSolid />,
+    name: "Tours",
+  },
+  {
+    icon: <FaCarSide />,
+    name: "Cars",
+  },
+  {
+    icon: <GrVisa />,
+    name: "Visa",
+  },
+  {
+    icon: <MdOutlineFlight />,
+    name: "Blogs",
+  },
+];
