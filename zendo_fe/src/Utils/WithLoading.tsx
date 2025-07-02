005801d32b8414b4c3651b22c95ec56908c93ed0
moshaosama <mohamedOSFekry@gmail.com>
2025-07-02 04:12:23 +0300

    add: utility functions added to Utils folder

---
+++ zendo_fe/src/Utils/WithLoading.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState, type ComponentType } from "react";
+import Loading from "../Components/loading";
+
+export function withLoading(WrappedComponent: ComponentType) {
+  return (props: any) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+      const handleSetLoader = async () => {
+        setIsLoading(true);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        setIsLoading(false);
+      };
+
+      handleSetLoader();
+    }, []);
+
+    return isLoading ? <Loading /> : <WrappedComponent {...props} />;
+  };
+}
