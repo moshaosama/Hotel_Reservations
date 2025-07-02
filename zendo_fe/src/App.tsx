5833990c1c881dd1496f6e2e17d97b05ac016539
moshaosama <mohamedOSFekry@gmail.com>
2025-07-02 04:12:13 +0300

    modify: updated App.tsx with routing and component integration

--- zendo_fe/src/App.tsx
+++ zendo_fe/src/App.tsx
@@ -1,17 +1,9 @@
-import DetailsCard from "./Features/Home/Components/DetailsCard";
-import FeatureHotels from "./Features/Home/Components/FeatureHotels";
-import Hero from "./Features/Home/Components/Hero";
-import Footer from "./Layouts/Footer";
-import Navbar from "./Layouts/Navbar";
+import Router from "./Router/Router";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <FeatureHotels />
-      <DetailsCard />
-      <Footer />
+      <Router />
     </>
   );
 }
