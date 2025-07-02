c64d21a8c0c3714421e5908c5929398f0dbea3e7
moshaosama <mohamedOSFekry@gmail.com>
2025-07-02 04:12:22 +0300

    add: HeroPages component for page-specific headers

---
+++ zendo_fe/src/Components/HeroPages.tsx
@@ -0,0 +1,29 @@
+import type { ReactElement } from "react";
+
+const HeroPages = ({
+  ImageSrc,
+  Title,
+  Component,
+}: {
+  ImageSrc: string;
+  Title: string;
+  Component: ReactElement;
+}) => {
+  return (
+    <>
+      <div className="trelative">
+        <img
+          src={ImageSrc}
+          alt="hero.png"
+          className="w-full h-[30pc] object-cover rounded-b-[10pc]"
+        />
+        <div className="absolute top-40 flex flex-col items-center w-full justify-center">
+          <h1 className="text-white text-4xl font-bold">{Title}</h1>
+          {Component}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroPages;
