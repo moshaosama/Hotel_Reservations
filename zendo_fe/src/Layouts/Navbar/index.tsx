bbd8c693e2dbc0b697eb037a733afbdbf90bee44
moshaosama <mohamedOSFekry@gmail.com>
2025-07-03 21:41:26 +0300

    modify: updated Navbar with new links or behavior

--- zendo_fe/src/Layouts/Navbar/index.tsx
+++ zendo_fe/src/Layouts/Navbar/index.tsx
@@ -3,8 +3,15 @@ import { NavbarLinks } from "../../Data/NavbarLinks";
 import { cn } from "../../libs/cn";
 import Container from "../../Styles";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
+import DeopDownHistory from "../../Components/DeopDownHistory";
+import { useDropDownHistoryContext } from "../../Context/DropDownHistoryContext";
+import useGetHistories from "../../Hooks/useGetHistories";
 
 const Navbar = () => {
+  const { isOpenHistory, handleTriggerOpenHistory } =
+    useDropDownHistoryContext();
+  const { History } = useGetHistories();
   return (
     <div className="bg-white sticky top-0 z-50 py-2 w-full">
       <div className={cn(Container, "flex justify-between items-center")}>
@@ -25,7 +32,26 @@ const Navbar = () => {
             </div>
           ))}
         </div>
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-2">
+          <div className="relative">
+            <div
+              onClick={handleTriggerOpenHistory}
+              className="flex gap-2 items-center border-1 border-gray-300 hover:border-blue-500 cursor-pointer px-4 py-2 rounded-2xl"
+            >
+              <i>
+                <FaSearch color="black" />
+              </i>
+              <h1 className="text-md font-bold">searches</h1>
+              <h1 className="text-md font-bold">{History?.length}</h1>
+              <i>
+                <IoMdArrowDropdown />
+              </i>
+            </div>
+            <div className="absolute">
+              {isOpenHistory && <DeopDownHistory />}
+            </div>
+          </div>
+
           <div className="flex gap-2 items-center border-1 border-gray-300 hover:border-blue-500 cursor-pointer px-4 py-2 rounded-2xl">
             <img
               src="/Flag_of_the_United_States_(Web_Colors).svg.png"
