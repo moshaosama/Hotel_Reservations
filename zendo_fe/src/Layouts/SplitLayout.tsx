7ff9a3fed8deb97f70a2bfea0b45c5bc66c24dd6
moshaosama <mohamedOSFekry@gmail.com>
2025-07-03 21:41:26 +0300

    modify: refined SplitLayout structure or layout handling

--- zendo_fe/src/Layouts/SplitLayout.tsx
+++ zendo_fe/src/Layouts/SplitLayout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
+import DropDownHistoryProvider from "../Context/DropDownHistoryContext";
 
 const SplitLayout = () => {
   return (
     <>
-      <Navbar />
+      <DropDownHistoryProvider>
+        <Navbar />
+      </DropDownHistoryProvider>
       <Outlet />
       <Footer />
     </>
