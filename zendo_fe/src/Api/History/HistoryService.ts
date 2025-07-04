bafcc1cec6c56e8937bf3bd0d778b9c092b5aac0
moshaosama <mohamedOSFekry@gmail.com>
2025-07-04 05:17:51 +0300

    modify: updated HistoryService with improved API logic

--- zendo_fe/src/Api/History/HistoryService.ts
+++ zendo_fe/src/Api/History/HistoryService.ts
@@ -1,10 +1,26 @@
 import axios from "axios";
 import { ParentService } from "../ParentService";
 
+interface DataBody {
+  pathName: string;
+  hotel_name: string;
+  checkIn: string;
+  checkOut: string;
+}
+
 class HistoryService extends ParentService {
   async getData(_?: any, thunkApi?: any): Promise<any> {
     try {
       const response = await axios.get("http://localhost:3000/history");
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+
+  async CreateHistory(data: DataBody, thunkApi?: any) {
+    try {
+      const response = await axios.post("http://localhost:3000/history", data);
       return response.data;
     } catch (err) {
       return thunkApi.rejectWithValue(err);
