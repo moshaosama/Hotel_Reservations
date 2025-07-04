4bcd134b8614bdb418ed5b68a45bc404a3a73403
moshaosama <mohamedOSFekry@gmail.com>
2025-07-04 05:17:51 +0300

    modify: enhanced hotel filtering logic in useFilterHotels hook

--- zendo_fe/src/Features/Hotels/Hooks/useFilterHotels.ts
+++ zendo_fe/src/Features/Hotels/Hooks/useFilterHotels.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { hotelService } from "../../../Api/Hotels/HotelService";
 import { useNavigate, useParams } from "react-router";
+import { historyService } from "../../../Api/History/HistoryService";
 const useFilterHotels = () => {
   const { register, handleSubmit } = useForm();
   const { hotel_name } = useParams();
@@ -11,11 +12,23 @@ const useFilterHotels = () => {
     queryFn: () => hotelService.getHotelByCity(hotel_name as string),
     enabled: !!hotel_name,
   });
-  const handleFilterationHotel = (data?: any) => {
+  const handleFilterationHotel = async (data?: any) => {
     Navigate(`/hotels/${data?.hotel_name}`);
+    await historyService.CreateHistory({
+      pathName: "Hotels",
+      hotel_name: data?.hotel_name,
+      checkIn: "2025-01-02",
+      checkOut: "2025-01-29",
+    });
   };
 
-  return { register, handleFilterationHotel, handleSubmit, hotelByName, isLoading };
+  return {
+    register,
+    handleFilterationHotel,
+    handleSubmit,
+    hotelByName,
+    isLoading,
+  };
 };
 
 export default useFilterHotels;
