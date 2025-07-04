0817bf6099376a076545561e480cea83be9d7337
moshaosama <mohamedOSFekry@gmail.com>
2025-07-04 18:00:17 +0300

    modify: adjusted Navbar layout or links

--- zendo_fe/src/Layouts/Navbar/index.tsx
+++ zendo_fe/src/Layouts/Navbar/index.tsx
@@ -71,7 +71,7 @@ const Navbar = () => {
             <h1>Login</h1>
           </div>
 
-          <div className="flex gap-2 items-center transition-all duration-500 font-bold bg-gray-300 hover:bg-blue-500 hover:text-white cursor-pointer px-4 py-2 rounded-2xl">
+          <div className="flex gap-2 items-center transition-all duration-500 font-bold bg-[#eef4fb] hover:bg-blue-500 hover:text-white cursor-pointer px-4 py-2 rounded-2xl">
             <h1>Sign Up</h1>
           </div>
         </div>
