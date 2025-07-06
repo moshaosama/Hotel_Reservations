02f6621f9669660787bf0a1852e77f75c8582390
moshaosama <mohamedOSFekry@gmail.com>
2025-07-06 20:04:39 +0300

    modify: enhanced useGetHistories hook for better data fetching

--- zendo_fe/src/Hooks/useGetHistories.ts
+++ zendo_fe/src/Hooks/useGetHistories.ts
@@ -1,13 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { historyService } from "../Api/History/HistoryService";
 
 const useGetHistories = () => {
-  const { data: History } = useQuery({
+  const { data: History, refetch } = useQuery({
     queryKey: ["history"],
     queryFn: historyService.getData,
   });
 
-  return { History };
+  const { isSuccess: isSuccessDelete, mutate: DeleteAllHistory } = useMutation({
+    mutationKey: ["deleteHistory"],
+    mutationFn: historyService.DeleteHistories,
+
+    onSuccess: () => {
+      refetch();
+    },
+  });
+
+  return { History, isSuccessDelete, DeleteAllHistory };
 };
 
 export default useGetHistories;
