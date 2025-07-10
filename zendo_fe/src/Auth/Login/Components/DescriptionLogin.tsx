f4c6b937fdd4387fb28d98f9ad9844f4fd4ba3a2
moshaosama <mohamedOSFekry@gmail.com>
2025-07-10 20:16:09 +0300

    add: added authentication module (Auth folder)

---
+++ zendo_fe/src/Auth/Login/Components/DescriptionLogin.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DescriptionLogin = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Login</h1>
+      <p className="text-gray-500 ">
+        Welcome back! Please sign in to your account
+      </p>
+    </div>
+  );
+};
+
+export default DescriptionLogin;
---
+++ zendo_fe/src/Auth/Login/Components/FormLogin.tsx
@@ -0,0 +1,53 @@
+import Button from "../../../Components/Button";
+import DescriptionLogin from "./DescriptionLogin";
+
+const FormLogin = () => {
+  return (
+    <div className="h-[90vh] flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-3 border-2 border-gray-200 w-[35pc] p-2 rounded-xl shadow-lg">
+        <DescriptionLogin />
+        <form action="" className="grid grid-cols-2 gap-2">
+          <p className="col-span-2 flex flex-col gap-1">
+            <label className="mx-1 font-semibold">Email Address</label>
+            <input
+              type="email"
+              className="py-2 border-2 border-gray-200 rounded-xl"
+            />
+          </p>
+
+          <p className="col-span-2 flex flex-col gap-1">
+            <label className="mx-1 font-semibold">Password</label>
+            <input
+              type="password"
+              className="py-2 border-2 border-gray-200 rounded-xl"
+            />
+          </p>
+
+          <div className="flex justify-between col-span-2 mt-3 mx-2">
+            <p className="col-span-2 flex gap-2 ">
+              <input
+                type="checkbox"
+                className="py-2 border-2 border-gray-200 rounded-xl"
+              />
+              <label className="mx-1 font-semibold text-gray-500">
+                Remember me
+              </label>
+            </p>
+            <p>
+              <h1 className="text-blue-700 cursor-pointer hover:underline font-semibold">
+                Reset Password
+              </h1>
+            </p>
+          </div>
+
+          <p className="col-span-2 flex flex-col">
+            <Button bGround="#2563eb" color="white" Title="Login" />
+            <Button Title="SignUp" bGround="white" color="#2563eb" />
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormLogin;
