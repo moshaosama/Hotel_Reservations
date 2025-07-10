ba882e5f04f740ab3d3f40f3f9d33c1a26658068
moshaosama <mohamedOSFekry@gmail.com>
2025-07-10 20:16:02 +0300

    modify: adjusted Navbar links and styling

--- zendo_fe/src/Layouts/Navbar/index.tsx
+++ zendo_fe/src/Layouts/Navbar/index.tsx
@@ -68,11 +68,15 @@ const Navbar = () => {
           </div>
 
           <div className="text-md font-bold cursor-pointer">
-            <h1>Login</h1>
+            <Link to={"/login"}>
+              <h1>Login</h1>
+            </Link>
           </div>
 
           <div className="flex gap-2 items-center transition-all duration-500 font-bold bg-[#eef4fb] hover:bg-blue-500 hover:text-white cursor-pointer px-4 py-2 rounded-2xl">
-            <h1>Sign Up</h1>
+            <Link to={"/signup"}>
+              <h1>Sign up</h1>
+            </Link>
           </div>
         </div>
       </div>
