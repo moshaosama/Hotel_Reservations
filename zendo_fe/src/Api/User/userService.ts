91529a1e2b48e1597d30e539bd12922994196907
moshaosama <mohamedOSFekry@gmail.com>
2025-07-11 14:55:22 +0300

    add: created User API module for registration and user-related endpoints

---
+++ zendo_fe/src/Api/User/userService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+class UserService {
+  async SignUp(user: any, thunkApi?: any) {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/auth/signup`,
+        user
+      );
+      return response.data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err);
+    }
+  }
+}
+
+export const userService = new UserService();
