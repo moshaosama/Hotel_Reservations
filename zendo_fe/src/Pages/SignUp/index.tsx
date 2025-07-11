ff540af6f07d9d8660cc8f1575f19e6d8b33efd5
moshaosama <mohamedOSFekry@gmail.com>
2025-07-11 14:38:07 +0300

    add: added SignUp page for user registration

---
+++ zendo_fe/src/Pages/SignUp/index.tsx
@@ -0,0 +1,13 @@
+import FormSignUp from "../../Auth/SignUp/Component/FormSignUp";
+import DownloadApp from "../../Components/DownloadApp";
+
+const SignUp = () => {
+  return (
+    <>
+      <FormSignUp />
+      <DownloadApp />
+    </>
+  );
+};
+
+export default SignUp;
