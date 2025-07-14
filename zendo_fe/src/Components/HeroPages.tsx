5623aef43a72071a094aaf75fe13ac9e3ab8c3db
moshaosama <mohamedOSFekry@gmail.com>
2025-07-14 18:03:11 +0300

    modify: refined HeroPages section visuals

--- zendo_fe/src/Components/HeroPages.tsx
+++ zendo_fe/src/Components/HeroPages.tsx
@@ -17,7 +17,7 @@ const HeroPages = ({
           alt="hero.png"
           className="w-full h-[30pc] object-cover rounded-b-[10pc]"
         />
-        <div className="absolute top-40 flex flex-col items-center w-full justify-center">
+        <div className="absolute top-40 flex flex-col items-center w-full justify-center max-sm:hidden">
           <h1 className="text-white text-4xl font-bold">{Title}</h1>
           {Component}
         </div>
