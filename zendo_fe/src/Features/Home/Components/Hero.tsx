3446b95eddd7e3e1066d95e561094dd5904ba7e8
moshaosama <mohamedOSFekry@gmail.com>
2025-07-14 18:03:12 +0300

    modify: improved Hero section design for Home page

--- zendo_fe/src/Features/Home/Components/Hero.tsx
+++ zendo_fe/src/Features/Home/Components/Hero.tsx
@@ -12,19 +12,21 @@ const Hero = () => {
         <img
           src="https://phptravels.net/uploads/global/bg.png"
           alt="hero.png"
-          className="w-full"
+          className="w-full max-sm:h-[16pc]"
         />
         <div className="bg-gradient-to-r from-[#000000c5] to-[#00000075] absolute top-0 left-0 w-full h-full"></div>
 
-        <div className="absolute top-0  text-white flex flex-col justify-center items-center gap-3 w-full mt-14">
-          <h1 className="text-5xl font-bold">Your Trip Starts Here!</h1>
-          <p className="text-xl">
-            Let us help you plan your next journey — the one that will leave a
-            lifetime of memories.
-          </p>
+        <div className="absolute top-0  text-white flex flex-col justify-center items-center gap-3 w-full mt-14 max-sm:mt-3 max-sm:overflow-hidden">
+          <div className="flex justify-center flex-col items-center gap-3 max-sm:hidden">
+            <h1 className="text-5xl font-bold">Your Trip Starts Here!</h1>
+            <p className="text-xl">
+              Let us help you plan your next journey — the one that will leave a
+              lifetime of memories.
+            </p>
+          </div>
 
-          <div className=" bg-[#ffffff0d] border-2 mt-7 border-[#ffffff21] text-white font-bold text-lg rounded-full">
-            <div className="flex gap-5 p-[0.4pc] px-5">
+          <div className=" bg-[#ffffff0d] border-2 mt-7 max-sm:mt-0 border-[#ffffff21] text-white font-bold text-lg rounded-full max-sm:rounded-xl">
+            <div className="grid grid-cols-5 max-sm:grid-cols-2 gap-5 p-[0.4pc] px-5 max-sm:w-[24pc]">
               {NavbarLinks.slice(0, 5)?.map(
                 (link: { name: string; icon: ReactElement }) => (
                   <div
