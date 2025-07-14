3b0708523bd867fcda73a03e681b826ed5b23589
moshaosama <mohamedOSFekry@gmail.com>
2025-07-14 17:03:01 +0300

    modify: adjusted Button component props and behavior

--- zendo_fe/src/Components/Button.tsx
+++ zendo_fe/src/Components/Button.tsx
@@ -4,16 +4,20 @@ const Button = ({
   color,
   bGround,
   Title,
+  isDisable = false,
 }: {
   color: string;
   bGround: string;
   Title: string;
+  isDisable?: boolean;
 }) => {
   return (
     <>
       <button
+        disabled={isDisable}
         className={cn(
-          "block py-3 rounded-xl cursor-pointer text-lg mt-4 font-semibold hover:bg-black hover:text-white"
+          "block py-3 rounded-xl  text-lg mt-4 font-semibold hover:bg-black hover:text-white",
+          isDisable ? "cursor-not-allowed" : "cursor-pointer"
         )}
         style={{
           color: color,
